91363db2b701e688bbe4f475c2a672b730c23891
tamilzh <tamilzhchelvan@gmail.com>
2024-11-04 15:46:09 +0530

    vercel

--- server/index.js
+++ server/index.js
@@ -12,6 +12,8 @@ app.get('/api', (req, res) => {
   res.json({ message: 'Hello from the backend!' });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}
